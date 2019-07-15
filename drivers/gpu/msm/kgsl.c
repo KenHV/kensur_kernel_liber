a4969585a7818e3157991debd22bb464f6fbdc3d
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-16 03:07:37 +0900

    adreno: leave only /sys/kernel/debug/kgsl/proc/%d/mem for debugfs
    Others are unused
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Change-Id: Ib936661541ef685d5746a32bec2fbe1ffee1d2a5
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4905,9 +4905,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	if (status)
 		return status;
 
-	/* Initialize logging first, so that failures below actually print. */
-	kgsl_device_debugfs_init(device);
-
 	/* Disable the sparse ioctl invocation as they are not used */
 	device->flags &= ~KGSL_FLAG_SPARSE;
 
@@ -5039,7 +5036,6 @@ error_close_mmu:
 error_pwrctrl_close:
 	kgsl_pwrctrl_close(device);
 error:
-	kgsl_device_debugfs_close(device);
 	_unregister_device(device);
 	return status;
 }
@@ -5065,7 +5061,6 @@ void kgsl_device_platform_remove(struct kgsl_device *device)
 
 	kgsl_pwrctrl_close(device);
 
-	kgsl_device_debugfs_close(device);
 	_unregister_device(device);
 }
 EXPORT_SYMBOL(kgsl_device_platform_remove);
