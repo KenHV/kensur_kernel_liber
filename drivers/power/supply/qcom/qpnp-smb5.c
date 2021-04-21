62eebe917c57c0ea54e8eb25d64dd14098bd6b86
KenHV <yo@kenharris.xyz>
2021-04-21 19:30:14 +0530

    power: supply: qcom: Silence verbose logging
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -2088,7 +2088,6 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		rc = smblib_get_prop_batt_age(chg, val);
 		break;
 	default:
-		pr_err("batt power supply prop %d not supported\n", psp);
 		return -EINVAL;
 	}
 
