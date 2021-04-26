kdrag0n/proton_kernel_wahoo

crypto: xts: Rename free() to xts_free()
GCC 10 doesn't like it when reserved function names are used:
crypto/xts.c:337:13: error: conflicting types for built-in function 'free'; expected 'void(void *)' [-Werror=builtin-declaration-mismatch]
  337 | static void free(struct crypto_instance *inst)
      |             ^~~~
crypto/xts.c:27:1: note: 'free' is declared in header '<stdlib.h>'
   26 | #include <crypto/gf128mul.h>
  +++ |+#include <stdlib.h>
   27 |

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

crypto/xts.c
@@ -334,7 +334,7 @@ out_put_alg:
 	return inst;
 }
 
-static void free(struct crypto_instance *inst)
+static void xts_free(struct crypto_instance *inst)
 {
 	crypto_drop_spawn(crypto_instance_ctx(inst));
 	kfree(inst);
@@ -343,7 +343,7 @@ static void free(struct crypto_instance *inst)
 static struct crypto_template crypto_tmpl = {
 	.name = "xts",
 	.alloc = alloc,
-	.free = free,
+	.free = xts_free,
 	.module = THIS_MODULE,
 };
 

KenHV/kensur_kernel_liber

Signed-off-by: KenHV <yo@kenharris.xyz>

crypto/xts.c
@@ -469,7 +469,7 @@ static void exit_tfm(struct crypto_skcipher *tfm)
 	crypto_free_cipher(ctx->tweak);
 }
 
-static void free(struct skcipher_instance *inst)
+static void xts_free(struct skcipher_instance *inst)
 {
 	crypto_drop_skcipher(skcipher_instance_ctx(inst));
 	kfree(inst);
@@ -580,7 +580,7 @@ static int create(struct crypto_template *tmpl, struct rtattr **tb)
 	inst->alg.encrypt = encrypt;
 	inst->alg.decrypt = decrypt;
 
-	inst->free = free;
+	inst->free = xts_free;
 
 	err = skcipher_register_instance(tmpl, inst);
 	if (err)