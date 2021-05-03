0b1cd35deb30d2015cf6c143917b871c79bb7508
KenHV <yo@kenharris.xyz>
2021-05-03 13:17:49 +0530

    power: wakeup: Log wakeup reason only if debugging is enabled
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -941,7 +941,9 @@ void pm_system_irq_wakeup(unsigned int irq_number)
 					irq_number, name);
 
 		}
+#ifdef CONFIG_PM_DEBUG
 		log_wakeup_reason(irq_number);
+#endif
 		pm_wakeup_irq = irq_number;
 		pm_system_wakeup();
 	}
