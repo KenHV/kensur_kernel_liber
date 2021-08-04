6b7484126a898bfc57a8bb4f1f8c3fb359430d17
KenHV <yo@kenharris.xyz>
2021-08-04 13:08:14 +0530

    include/usb: Fix mismerge
    Why yes, I am a gitretard, how could you tell?
    
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- include/linux/usb/usbnet.h
+++ include/linux/usb/usbnet.h
@@ -87,13 +87,10 @@ struct usbnet {
 #		define EVENT_LINK_CHANGE	11
 #		define EVENT_SET_RX_MODE	12
 #		define EVENT_NO_IP_ALIGN	13
-<<<<<<< HEAD
-	u32			rx_speed;	/* in bps - NOT Mbps */
-	u32			tx_speed;	/* in bps - NOT Mbps */
-=======
 #		define USBNET_DISCONNECT	14
 #		define USBNET_LINK		15
->>>>>>> 88a1c55fd8be45a6299be4c3b76c0e22efc5c465
+	u32			rx_speed;	/* in bps - NOT Mbps */
+	u32			tx_speed;	/* in bps - NOT Mbps */
 	void			*ipc_log_ctxt;
 	int			netdev_id;
 
