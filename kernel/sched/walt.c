YumeMichi/kernel_xiaomi_dipper

sched: cpufreq: Stop ignoring util updates
Eliminate the check for SCHED_CPUFREQ_WALT flag in
cpufreq_update_util(), update calling code to stop using that flag,
and replace its definition with a placeholder.

Test: Trace shows more frequent util updates
Bug: 110604715
Change-Id: I3b74e950b984194f08ecbbf91872467a200c9d1d
Signed-off-by: Connor O'Brien <connoro@google.com>

kernel/sched/walt.c
@@ -3246,7 +3246,6 @@ void walt_irq_work(struct irq_work *irq_work)
 	struct rq *rq;
 	int cpu;
 	u64 wc, total_grp_load = 0;
-	int flag = SCHED_CPUFREQ_WALT;
 	bool is_migration = false;
 	int level = 0;
 
@@ -3292,16 +3291,16 @@ void walt_irq_work(struct irq_work *irq_work)
 
 	for_each_sched_cluster(cluster) {
 		for_each_cpu(cpu, &cluster->cpus) {
-			int nflag = flag;
+			int nflag = 0;
 
 			rq = cpu_rq(cpu);
 
 			if (is_migration) {
 				if (rq->notif_pending) {
-					nflag |= SCHED_CPUFREQ_INTERCLUSTER_MIG;
+					nflag = SCHED_CPUFREQ_INTERCLUSTER_MIG;
 					rq->notif_pending = false;
 				} else {
-					nflag |= SCHED_CPUFREQ_FORCE_UPDATE;
+					nflag = SCHED_CPUFREQ_FORCE_UPDATE;
 				}
 			}
 

KenHV/kensur_kernel_liber

Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

kernel/sched/walt.c
@@ -3215,16 +3215,16 @@ void walt_irq_work(struct irq_work *irq_work)
 						cpu_online_mask);
 		num_cpus = cpumask_weight(&cluster_online_cpus);
 		for_each_cpu(cpu, &cluster_online_cpus) {
-			int flag = SCHED_CPUFREQ_WALT;
+			int flag = 0;
 
 			rq = cpu_rq(cpu);
 
 			if (is_migration) {
 				if (rq->notif_pending) {
-					flag |= SCHED_CPUFREQ_INTERCLUSTER_MIG;
+					flag = SCHED_CPUFREQ_INTERCLUSTER_MIG;
 					rq->notif_pending = false;
 				} else
-					flag |= SCHED_CPUFREQ_FORCE_UPDATE;
+					flag = SCHED_CPUFREQ_FORCE_UPDATE;
 			}
 
 			if (i == num_cpus)